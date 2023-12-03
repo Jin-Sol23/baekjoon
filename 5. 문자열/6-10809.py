3b940071ab2c22f59aeaf9537a558754085fcf3f
Jin-Sol23 <ljs121345@gmail.com>
2023-12-03 16:26:24 +0900

    baekjoon

---
+++ 5. 문자열/6-10809.py
@@ -0,0 +1,11 @@
+# 소문자 a = 97, z= 122
+s = input()
+a = ord("a")
+z = ord("z")
+
+for i in range(97, 123):
+    for j in range(chr(s)):
+        if i == j:
+            i = 
+            print(len(i))
+    print(i)
