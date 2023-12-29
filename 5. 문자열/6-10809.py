a543b8570cef4213191d23585d9bee9242c5380f
Jin-Sol23 <135953434+Jin-Sol23@users.noreply.github.com>
2023-12-29 18:35:42 +0900

    Add files via upload

--- 5. 문자열/6-10809.py
+++ 5. 문자열/6-10809.py
@@ -9,3 +9,41 @@ for i in range(97, 123):
             i = 
             print(len(i))
     print(i)
+#%%
+s = 'baekjoon'
+for j in range(97, 123):
+    found = False
+    for i in range(len(s)):
+        if s[i] == chr(j):
+            # j = i
+            found = True
+#            break
+    if not found:
+        i = -1
+    print(i, end=' ')
+
+# found sw 의 역할
+# found sw 없이 이 문제 풀어보기
+#%%
+
+s = 'baekjoon'
+for j in range(97, 123):
+    # found = False
+    for i in range(len(s)):
+        
+        print(chr(j),j, s[i],  s[i] == chr(j), i)
+        
+        # if s[i] == chr(j):
+            # j = i
+            # found = True
+#            break
+    # if not found:
+    #     i = -1
+    # print(i, end=' ')
+
+
+
+
+
+
+
